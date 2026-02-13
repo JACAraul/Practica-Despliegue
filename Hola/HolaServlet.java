77ff0321c4c3ee46c61554f62c38d73bf5e019ff
JACAraul <rcalderong02@educantabria.es>
2026-02-13 13:33:13 +0100

    a

--- Hola/HolaServlet.java
+++ Hola/HolaServlet.java
@@ -1,6 +1,10 @@
 package hola;
 
-import jakarta.*;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;  
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.*;
 
 @WebServlet("/HolaServlet")                    
